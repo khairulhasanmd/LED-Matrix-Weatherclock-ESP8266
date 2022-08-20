39efa0938cc2199ed6efa2abcdafca87e3d9e4d0
Md. Khairul Hasan <khairulhasanmd@gmail.com>
2022-08-20 18:09:23 +0600

    file formatted

--- src/main.cpp
+++ src/main.cpp
@@ -5,18 +5,27 @@
  * ArduinoJson at version 5.13.2
   Download these library from library manager.
 */
+#include "max7219.h"
+#include "fonts.h"
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <ArduinoJson.h>
 #include <TimeLib.h>
-
 #include <WiFiManager.h>
-WiFiManager wifiManager;
-
 #include <ESPAsyncTCP.h>
 
-WiFiClient client;
+// for ESP-01 module
+//#define DIN_PIN 2 // D4
+//#define CS_PIN  3 // D9/RX
+//#define CLK_PIN 0 // D3
+// for NodeMCU 1.0
+#define DIN_PIN D7
+#define CS_PIN D4
+#define CLK_PIN D5
+#define NUM_MAX 4
 
+WiFiManager wifiManager;
+WiFiClient client;
 String weatherMain = "";
 String weatherDescription = "";
 String weatherLocation = "";
@@ -28,30 +37,14 @@ float tempMin, tempMax;
 int clouds;
 float windSpeed;
 String date;
-
 String currencyRates;
 String weatherString;
-
-#define NUM_MAX 4
-
-// for ESP-01 module
-//#define DIN_PIN 2 // D4
-//#define CS_PIN  3 // D9/RX
-//#define CLK_PIN 0 // D3
-
-// for NodeMCU 1.0
-#define DIN_PIN D7
-#define CS_PIN  D4
-#define CLK_PIN D5
-
-#include "max7219.h"
-#include "fonts.h"
 
 // =======================================================================
 // CHANGE YOUR CONFIG HERE:
 // =======================================================================
-const char* ssid     = "AXIOM-Office";     // SSID of local network
-const char* password = "bismillah";   // Password on network
+const char * ssid = "AXIOM-Office"; // SSID of local network
+const char * password = "bismillah"; // Password on network
 String weatherKey = ""; // city id from openweather 
 String weatherLang = "&lang=en";
 String cityID = "1337233"; // Cityid from open weather
@@ -60,113 +53,190 @@ String cityID = "1337233"; // Cityid from open weather
 
 // =======================================================================
 #define MAX_DIGITS 16
-byte dig[MAX_DIGITS]={0};
-byte digold[MAX_DIGITS]={0};
-byte digtrans[MAX_DIGITS]={0};
+byte dig[MAX_DIGITS] = {
+  0
+};
+byte digold[MAX_DIGITS] = {
+  0
+};
+byte digtrans[MAX_DIGITS] = {
+  0
+};
 int updCnt = 0;
 int dots = 0;
 long dotTime = 0;
 long clkTime = 0;
-int dx=0;
-int dy=0;
-byte del=0;
-int h,m,s;
+int dx = 0;
+int dy = 0;
+byte del = 0;
+int h, m, s;
+const char * weatherHost = "api.openweathermap.org";
 // =======================================================================
 
-
-
 // =======================================================================
 
-void showDigit(char ch, int col, const uint8_t *data)
-{
-  if(dy<-8 | dy>8) return;
+void showDigit(char ch, int col,
+  const uint8_t * data) {
+  if (dy < -8 | dy > 8) return;
   int len = pgm_read_byte(data);
   int w = pgm_read_byte(data + 1 + ch * len);
   col += dx;
   for (int i = 0; i < w; i++)
-    if(col+i>=0 && col+i<8*NUM_MAX) {
+    if (col + i >= 0 && col + i < 8 * NUM_MAX) {
       byte v = pgm_read_byte(data + 1 + ch * len + 1 + i);
-      if(!dy) scr[col + i] = v; else scr[col + i] |= dy>0 ? v>>dy : v<<-dy;
+      if (!dy) scr[col + i] = v;
+      else scr[col + i] |= dy > 0 ? v >> dy : v << -dy;
     }
 }
 
 // =======================================================================
 
-void setCol(int col, byte v)
-{
-  if(dy<-8 | dy>8) return;
+void setCol(int col, byte v) {
+  if (dy < -8 | dy > 8) return;
   col += dx;
-  if(col>=0 && col<8*NUM_MAX)
-    if(!dy) scr[col] = v; else scr[col] |= dy>0 ? v>>dy : v<<-dy;
+  if (col >= 0 && col < 8 * NUM_MAX)
+    if (!dy) scr[col] = v;
+    else scr[col] |= dy > 0 ? v >> dy : v << -dy;
 }
 
 // =======================================================================
 
-int showChar(char ch, const uint8_t *data)
-{
+int showChar(char ch,
+  const uint8_t * data) {
   int len = pgm_read_byte(data);
-  int i,w = pgm_read_byte(data + 1 + ch * len);
+  int i, w = pgm_read_byte(data + 1 + ch * len);
   for (i = 0; i < w; i++)
-    scr[NUM_MAX*8 + i] = pgm_read_byte(data + 1 + ch * len + 1 + i);
-  scr[NUM_MAX*8 + i] = 0;
+    scr[NUM_MAX * 8 + i] = pgm_read_byte(data + 1 + ch * len + 1 + i);
+  scr[NUM_MAX * 8 + i] = 0;
   return w;
 }
 
 // =======================================================================
 int dualChar = 0;
 
-unsigned char convertPolish(unsigned char _c)
-{
+unsigned char convertPolish(unsigned char _c) {
   unsigned char c = _c;
-  if(c==196 || c==197 || c==195) {
+  if (c == 196 || c == 197 || c == 195) {
     dualChar = c;
     return 0;
   }
-  if(dualChar) {
-    switch(_c) {
-      case 133: c = 1+'~'; break; // 'ą'
-      case 135: c = 2+'~'; break; // 'ć'
-      case 153: c = 3+'~'; break; // 'ę'
-      case 130: c = 4+'~'; break; // 'ł'
-      case 132: c = dualChar==197 ? 5+'~' : 10+'~'; break; // 'ń' and 'Ą'
-      case 179: c = 6+'~'; break; // 'ó'
-      case 155: c = 7+'~'; break; // 'ś'
-      case 186: c = 8+'~'; break; // 'ź'
-      case 188: c = 9+'~'; break; // 'ż'
+  if (dualChar) {
+    switch (_c) {
+    case 133:
+      c = 1 + '~';
+      break; // 'ą'
+    case 135:
+      c = 2 + '~';
+      break; // 'ć'
+    case 153:
+      c = 3 + '~';
+      break; // 'ę'
+    case 130:
+      c = 4 + '~';
+      break; // 'ł'
+    case 132:
+      c = dualChar == 197 ? 5 + '~' : 10 + '~';
+      break; // 'ń' and 'Ą'
+    case 179:
+      c = 6 + '~';
+      break; // 'ó'
+    case 155:
+      c = 7 + '~';
+      break; // 'ś'
+    case 186:
+      c = 8 + '~';
+      break; // 'ź'
+    case 188:
+      c = 9 + '~';
+      break; // 'ż'
       //case 132: c = 10+'~'; break; // 'Ą'
-      case 134: c = 11+'~'; break; // 'Ć'
-      case 152: c = 12+'~'; break; // 'Ę'
-      case 129: c = 13+'~'; break; // 'Ł'
-      case 131: c = 14+'~'; break; // 'Ń'
-      case 147: c = 15+'~'; break; // 'Ó'
-      case 154: c = 16+'~'; break; // 'Ś'
-      case 185: c = 17+'~'; break; // 'Ź'
-      case 187: c = 18+'~'; break; // 'Ż'
-      default:  break;
+    case 134:
+      c = 11 + '~';
+      break; // 'Ć'
+    case 152:
+      c = 12 + '~';
+      break; // 'Ę'
+    case 129:
+      c = 13 + '~';
+      break; // 'Ł'
+    case 131:
+      c = 14 + '~';
+      break; // 'Ń'
+    case 147:
+      c = 15 + '~';
+      break; // 'Ó'
+    case 154:
+      c = 16 + '~';
+      break; // 'Ś'
+    case 185:
+      c = 17 + '~';
+      break; // 'Ź'
+    case 187:
+      c = 18 + '~';
+      break; // 'Ż'
+    default:
+      break;
     }
     dualChar = 0;
     return c;
-  }    
-  switch(_c) {
-    case 185: c = 1+'~'; break;
-    case 230: c = 2+'~'; break;
-    case 234: c = 3+'~'; break;
-    case 179: c = 4+'~'; break;
-    case 241: c = 5+'~'; break;
-    case 243: c = 6+'~'; break;
-    case 156: c = 7+'~'; break;
-    case 159: c = 8+'~'; break;
-    case 191: c = 9+'~'; break;
-    case 165: c = 10+'~'; break;
-    case 198: c = 11+'~'; break;
-    case 202: c = 12+'~'; break;
-    case 163: c = 13+'~'; break;
-    case 209: c = 14+'~'; break;
-    case 211: c = 15+'~'; break;
-    case 140: c = 16+'~'; break;
-    case 143: c = 17+'~'; break;
-    case 175: c = 18+'~'; break;
-    default:  break;
+  }
+  switch (_c) {
+  case 185:
+    c = 1 + '~';
+    break;
+  case 230:
+    c = 2 + '~';
+    break;
+  case 234:
+    c = 3 + '~';
+    break;
+  case 179:
+    c = 4 + '~';
+    break;
+  case 241:
+    c = 5 + '~';
+    break;
+  case 243:
+    c = 6 + '~';
+    break;
+  case 156:
+    c = 7 + '~';
+    break;
+  case 159:
+    c = 8 + '~';
+    break;
+  case 191:
+    c = 9 + '~';
+    break;
+  case 165:
+    c = 10 + '~';
+    break;
+  case 198:
+    c = 11 + '~';
+    break;
+  case 202:
+    c = 12 + '~';
+    break;
+  case 163:
+    c = 13 + '~';
+    break;
+  case 209:
+    c = 14 + '~';
+    break;
+  case 211:
+    c = 15 + '~';
+    break;
+  case 140:
+    c = 16 + '~';
+    break;
+  case 143:
+    c = 17 + '~';
+    break;
+  case 175:
+    c = 18 + '~';
+    break;
+  default:
+    break;
   }
   return c;
 }
@@ -175,10 +245,10 @@ unsigned char convertPolish(unsigned char _c)
 
 void printCharWithShift(unsigned char c, int shiftDelay) {
   c = convertPolish(c);
-  if (c < ' ' || c > '~'+25) return;
+  if (c < ' ' || c > '~' + 25) return;
   c -= 32;
   int w = showChar(c, font);
-  for (int i=0; i<w+1; i++) {
+  for (int i = 0; i < w + 1; i++) {
     delay(shiftDelay);
     scrollLeft();
     refreshAll();
@@ -187,30 +257,22 @@ void printCharWithShift(unsigned char c, int shiftDelay) {
 
 // =======================================================================
 
-void printStringWithShift(const char* s, int shiftDelay){
-  while (*s) {
-    printCharWithShift(*s, shiftDelay);
+void printStringWithShift(const char * s, int shiftDelay) {
+  while ( * s) {
+    printCharWithShift( * s, shiftDelay);
     s++;
   }
 }
 
 // =======================================================================
 
-const char *weatherHost = "api.openweathermap.org";
-
-
-
-
-
-
-
-void getWeatherData()
-{
-  Serial.print("connecting to "); Serial.println(weatherHost);
+void getWeatherData() {
+  Serial.print("connecting to ");
+  Serial.println(weatherHost);
   if (client.connect(weatherHost, 80)) {
     client.println(String("GET /data/2.5/weather?id=") + cityID + "&units=metric&appid=" + weatherKey + weatherLang + "\r\n" +
-                "Host: " + weatherHost + "\r\nUser-Agent: ArduinoWiFi/1.1\r\n" +
-                "Connection: close\r\n\r\n");
+      "Host: " + weatherHost + "\r\nUser-Agent: ArduinoWiFi/1.1\r\n" +
+      "Connection: close\r\n\r\n");
   } else {
     Serial.println("connection failed");
     return;
@@ -238,14 +300,13 @@ void getWeatherData()
   Serial.println(line);
 
   DynamicJsonBuffer jsonBuf;
-  JsonObject &root = jsonBuf.parseObject(line);
-  if (!root.success())
-  {
+  JsonObject & root = jsonBuf.parseObject(line);
+  if (!root.success()) {
     Serial.println("parseObject() failed");
     return;
   }
   //weatherMain = root["weather"]["main"].as<String>();
-  weatherDescription = root["weather"]["description"].as<String>();
+  weatherDescription = root["weather"]["description"].as < String > ();
   weatherDescription.toLowerCase();
   //  weatherLocation = root["name"].as<String>();
   //  country = root["sys"]["country"].as<String>();
@@ -256,31 +317,24 @@ void getWeatherData()
   tempMax = root["main"]["temp_max"];
   windSpeed = root["wind"]["speed"];
   clouds = root["clouds"]["all"];
-  String deg = String(char('~'+25));
-  weatherString = "         Temp: " + String(temp,1) + deg + "C (" + String(tempMin,1) + deg + "-" + String(tempMax,1) + deg + ")  ";
+  String deg = String(char('~' + 25));
+  weatherString = "         Temp: " + String(temp, 1) + deg + "C (" + String(tempMin, 1) + deg + "-" + String(tempMax, 1) + deg + ")  ";
   weatherString += weatherDescription;
   weatherString += "  Humidity: " + String(humidity) + "%  ";
   weatherString += "  Pressure: " + String(pressure) + "hPa  ";
   weatherString += "  Clouds: " + String(clouds) + "%  ";
-  weatherString += "  Wind: " + String(windSpeed,1) + "m/s                 ";
+  weatherString += "  Wind: " + String(windSpeed, 1) + "m/s                 ";
 }
 
-
-
-
-
+// =======================================================================
 
 // =======================================================================
 
-
-// =======================================================================
-
-float utcOffset = 5.50;  //Change utcoffset according to your utc any +- varry a little bit adjust it as per the needs.
+float utcOffset = 5.50; //Change utcoffset according to your utc any +- varry a little bit adjust it as per the needs.
 long localEpoc = 0;
 long localMillisAtUpdate = 0;
 
-void getTime()
-{
+void getTime() {
   WiFiClient client;
   if (!client.connect("www.google.com", 80)) {
     Serial.println("connection to google failed");
@@ -288,8 +342,8 @@ void getTime()
   }
 
   client.print(String("GET / HTTP/1.1\r\n") +
-               String("Host: www.google.com\r\n") +
-               String("Connection: close\r\n\r\n"));
+    String("Host: www.google.com\r\n") +
+    String("Connection: close\r\n\r\n"));
   int repeatCounter = 0;
   while (!client.available() && repeatCounter < 10) {
     delay(500);
@@ -299,16 +353,16 @@ void getTime()
 
   String line;
   client.setNoDelay(false);
-  while(client.connected() && client.available()) {
+  while (client.connected() && client.available()) {
     line = client.readStringUntil('\n');
     line.toUpperCase();
     if (line.startsWith("DATE: ")) {
-      date = "     "+line.substring(6, 22);
+      date = "     " + line.substring(6, 22);
       h = line.substring(23, 25).toInt();
       m = line.substring(26, 28).toInt();
       s = line.substring(29, 31).toInt();
       localMillisAtUpdate = millis();
-      localEpoc = ((h+6) * 60 * 60 + m * 60 + s);
+      localEpoc = ((h + 6) * 60 * 60 + m * 60 + s);
     }
   }
   client.stop();
@@ -316,8 +370,7 @@ void getTime()
 
 // =======================================================================
 
-void updateTime()
-{
+void updateTime() {
   // sprintf(buff, "%02d.%02d.%02d %02d:%02d:%02d", day(t), month(t), year(t), hour(t), minute(t), second(t));
   long curEpoch = localEpoc + ((millis() - localMillisAtUpdate) / 1000);
   // long epoch = (curEpoch + 3600 * utcOffset + 86400L) % 86400L;
@@ -327,102 +380,104 @@ void updateTime()
 }
 
 // =======================================================================
-void setup() 
-{
+void setup() {
   Serial.begin(115200);
-  printStringWithShift("Connecting WiFi   ",40);
+  printStringWithShift("Connecting WiFi   ", 40);
   wifiManager.autoConnect("64x8 LED Matrix Display");
 
   initMAX7219();
-  sendCmdAll(CMD_SHUTDOWN,1);
-  sendCmdAll(CMD_INTENSITY,1);
+  sendCmdAll(CMD_SHUTDOWN, 1);
+  sendCmdAll(CMD_INTENSITY, 1);
   Serial.println("");
-  Serial.print("Connected: "); 
+  Serial.print("Connected: ");
   Serial.println(WiFi.localIP());
 }
 
-
 // =======================================================================
 
-void showSimpleClock()
-{
-  dx=dy=0;
+void showSimpleClock() {
+  dx = dy = 0;
   clr();
-   if(h>12){
-    h=h-12;
-    }
-  showDigit(h/10,  0, dig4x8);
-  showDigit(h%10,  5, dig4x8);
-  showDigit(m/10, 12,  dig4x8);
-  showDigit(m%10, 17, dig4x8);
-  showDigit(s/10, 24, dig3x5);
-  showDigit(s%10, 28, dig3x5);
-  setCol(10,dots ? B00100100 : 0);
-  setCol(22,dots ? B00100100 : 0);
+  if (h > 12) {
+    h = h - 12;
+  }
+  showDigit(h / 10, 0, dig4x8);
+  showDigit(h % 10, 5, dig4x8);
+  showDigit(m / 10, 12, dig4x8);
+  showDigit(m % 10, 17, dig4x8);
+  showDigit(s / 10, 24, dig3x5);
+  showDigit(s % 10, 28, dig3x5);
+  setCol(10, dots ? B00100100 : 0);
+  setCol(22, dots ? B00100100 : 0);
   refreshAll();
 }
 
 // =======================================================================
 
-void showAnimClock()
-{
-  byte digPos[6]={0,5,12,17,24,28};
+void showAnimClock() {
+  byte digPos[6] = {
+    0,
+    5,
+    12,
+    17,
+    24,
+    28
+  };
   int digHt = 12;
-  int num = 6; 
+  int num = 6;
   int i;
-  if(del==0) {
+  if (del == 0) {
     del = digHt;
-    for(i=0; i<num; i++) digold[i] = dig[i];
-    dig[0] = h/10 ? h/10 : 10;
-    dig[1] = h%10;
-    dig[2] = m/10;
-    dig[3] = m%10;
-    dig[4] = s/10;
-    dig[5] = s%10;
-    for(i=0; i<num; i++)  digtrans[i] = (dig[i]==digold[i]) ? 0 : digHt;
+    for (i = 0; i < num; i++) digold[i] = dig[i];
+    dig[0] = h / 10 ? h / 10 : 10;
+    dig[1] = h % 10;
+    dig[2] = m / 10;
+    dig[3] = m % 10;
+    dig[4] = s / 10;
+    dig[5] = s % 10;
+    for (i = 0; i < num; i++) digtrans[i] = (dig[i] == digold[i]) ? 0 : digHt;
   } else
     del--;
-  
+
   clr();
-  for(i=0; i<num; i++) {
-    if(digtrans[i]==0) {
-      dy=0;
+  for (i = 0; i < num; i++) {
+    if (digtrans[i] == 0) {
+      dy = 0;
       showDigit(dig[i], digPos[i], dig3x8);
     } else {
-      dy = digHt-digtrans[i];
+      dy = digHt - digtrans[i];
       showDigit(digold[i], digPos[i], dig6x8);
       dy = -digtrans[i];
       showDigit(dig[i], digPos[i], dig6x8);
       digtrans[i]--;
     }
   }
-  dy=0;
-  setCol(10,dots ? B00100100 : 0);
-  setCol(22,dots ? B00100100 : 0);
+  dy = 0;
+  setCol(10, dots ? B00100100 : 0);
+  setCol(22, dots ? B00100100 : 0);
   refreshAll();
   delay(30);
 }
-void loop()
-{
-  if(updCnt<=0) { // every 10 scrolls, ~450s=7.5m
-    updCnt = 40;
+void loop() {
+  if (updCnt <= 0) { // every 10 scrolls, ~450s=7.5m
+    updCnt = 40; //was 10
     Serial.println("Getting data ...");
-    printStringWithShift("   Getting Data Wait   ",40);
+    printStringWithShift("   Getting Data Wait   ", 40);
     getWeatherData();
 
     getTime();
     Serial.println("Data loaded");
     clkTime = millis();
   }
- 
-  if(millis()-clkTime > 15000 && !del && dots) { // clock for 15s, then scrolls for about 30s
-    printStringWithShift(date.c_str(),40);
-    printStringWithShift(currencyRates.c_str(),35);
-    printStringWithShift(weatherString.c_str(),40);
+
+  if (millis() - clkTime > 15000 && !del && dots) { // clock for 15s, then scrolls for about 30s
+    printStringWithShift(date.c_str(), 40);
+    printStringWithShift(currencyRates.c_str(), 35);
+    printStringWithShift(weatherString.c_str(), 40);
     updCnt--;
     clkTime = millis();
   }
-  if(millis()-dotTime > 500) {
+  if (millis() - dotTime > 500) {
     dotTime = millis();
     dots = !dots;
   }
